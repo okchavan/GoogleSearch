cd42e220252b8c1fce4c85dea30d0b6f786fc349
okchavan <omkarchavan1105@gmail.com>
2023-07-23 00:10:53 +0530

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-    key: "AIzaSyB-NQ7bYD4Z1pdCb8JWS_sXjJWpvxpsrZY",
-    cx: "a35b5ad2e52d9457c",
+    key: "",
+    cx: "",
 };
 
 export const fetchDataFromApi = async (payload) => {
